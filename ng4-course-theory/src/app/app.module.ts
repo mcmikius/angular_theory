d6a122f56b289ec21c460508081aef33269f50a0
Michail Bondarenko <mcmikius15@gmail.com>
2019-03-09 09:18:14 +0200

    Create components ng g c name

--- ng4-course-theory/src/app/app.module.ts
+++ ng4-course-theory/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+//import { HttpModule } from '@angular/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyComponent} from './my/my.component';
+import { My2Component } from './my2/my2.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyComponent,
+    My2Component
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+//    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
