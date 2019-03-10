5057591d993b630baddc626c2c80793285586218
Michail Bondarenko <mcmikius15@gmail.com>
2019-03-10 15:36:22 +0200

    new

--- ng4-course-theory/src/app/app.module.ts
+++ ng4-course-theory/src/app/app.module.ts
@@ -4,14 +4,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MyComponent} from './my/my.component';
-import { My2Component } from './my2/my2.component'
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MyComponent,
-    My2Component
+    AppComponent
+
   ],
   imports: [
     BrowserModule,
